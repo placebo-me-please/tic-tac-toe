bd3eff7616dded2e21901b4e8b54c64e1c6454b8
placebo-me-please <alexander.c.perry@gmail.com>
2021-03-06 10:27:10 -0700

    Created grid that can be populated with values using the f-string literal method

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,18 @@
-print('hello world')
+#import os
+#os.system('clear')
+
+def print_board(grid_selection):
+	print(f'\
+		   |   |   \n\
+		 {grid_selection[0]} | {grid_selection[1]} | {grid_selection[2]} \n\
+		___|___|___\n\
+		   |   |   \n\
+		 {grid_selection[3]} | {grid_selection[4]} | {grid_selection[5]} \n\
+		___|___|___\n\
+		   |   |   \n\
+		 {grid_selection[6]} | {grid_selection[7]} | {grid_selection[8]} \n\
+		   |   |   \n')
+
+grid_selection = [' ',' ',' ',' ',' ',' ',' ',' ',' ',]
+print_board(grid_selection)
+

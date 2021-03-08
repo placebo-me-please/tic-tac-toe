3a79e4dfc61f9657ac42fed182e98187c8771dd0
placebo-me-please <alexander.c.perry@gmail.com>
2021-03-07 19:22:32 -0700

    Improved the interface between the validation function and the user input function, then added the ability to place a single 'X' on the grid

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,18 +4,34 @@
 def print_board(grid_selection):
 	print(f'\
 		   |   |   \n\
-		 {grid_selection[0]} | {grid_selection[1]} | {grid_selection[2]} \n\
+		 {grid_selection[6]} | {grid_selection[7]} | {grid_selection[8]} \n\
 		___|___|___\n\
 		   |   |   \n\
 		 {grid_selection[3]} | {grid_selection[4]} | {grid_selection[5]} \n\
 		___|___|___\n\
 		   |   |   \n\
-		 {grid_selection[6]} | {grid_selection[7]} | {grid_selection[8]} \n\
+		 {grid_selection[0]} | {grid_selection[1]} | {grid_selection[2]} \n\
 		   |   |   \n')
 
+def place_symbol(grid_selection):
+	player_selection = player_input()
+	grid_selection[player_selection - 1] = "X"
+	print_board(grid_selection)
+
 def player_input():
-	player_selection = input("Enter your selection (numeric value in the range [1,9]): ")
-	validate_selection(player_selection)
+	#selection status is assumed to be false unless the validation function passes the user selection
+	selection_status = False
+	while selection_status == False:
+		#this loop executes until the selection is recognized as valid
+		player_selection = input("Enter your selection (numeric value in the range [1,9]): ")
+		selection_status = validate_selection(player_selection)
+
+		#if the input is valid then this function returns the input an integer
+		if selection_status == True:
+			return int(player_selection)
+		#otherwise it ignores the input an the while loop starts again	
+		elif selection_status == False:
+			pass
 
 def validate_selection(player_selection):
 	#validation statuses are assumed to be False unless proven to be true
@@ -28,7 +44,7 @@ def validate_selection(player_selection):
 	elif player_selection.isdigit() == False:
 		print('Error: input was not numeric you Donkey')
 		validation_status[0] = False
-		return
+		return False
 
 	#checks if the value is within range (only if it is numeric)
 	if validation_status[0] == True and player_selection > 0  and player_selection < 10:
@@ -36,15 +52,28 @@ def validate_selection(player_selection):
 	elif player_selection >= 0 or player_selection >= 10:
 		print('Error: input was out of range you Donkey')
 		validation_status[1] = False
-		return
+		return False
 
 	#final validation check
 	if all(validation_status) == True:
-		return player_selection
+		return True
 
 #TESTING
-#this should prompt the user for an input and validate it before deciding to place it on the grid
-player_input()
+#this test should exercise the ability of the code to assess whether or not the board is fully populated
+#expected output here is False
+grid_selection = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+check_empty(grid_selection)
+#expected output here is True
+grid_selection = ['X','X','X','X','X','X','X','X','X']
+check_empty(grid_selection)
+
+
+#this should populate an "X" in whatever space the user selected on the grid
+# grid_selection = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+# place_symbol(grid_selection)
+
+# this should prompt the user for an input and validate it before deciding to place it on the grid
+# player_input()
 
 #this should validate if the value is numeric and within range, and return an appropriate error if one or both conditions are untrue
 # player_selection = ['0','5','10', 'A']
@@ -79,9 +108,9 @@ player_input()
 # player_input()
 
 #this should produce a 3x3 tic-tac-toe grid with only an 'X' in the upper-left corner
-#grid_selection = ['X',' ',' ',' ',' ',' ',' ',' ',' ',]
+#grid_selection = ['X',' ',' ',' ',' ',' ',' ',' ',' ']
 #print_board(grid_selection)
 
 #this should produce an empty 3x3 tic-tac-toe grid
-#grid_selection = [' ',' ',' ',' ',' ',' ',' ',' ',' ',]
+#grid_selection = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 #print_board(grid_selection)
